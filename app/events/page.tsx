1dd0cff73435cced57d0c1454177a2eda7d10c22
Aman <patelaman0241@gmail.com>
2025-12-07 12:23:22 +0530

    another try

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,30 +1,40 @@
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-import { IEvent } from "@/database";
-import { cacheLife } from "next/cache";
-import Eventcard from "@/components/Eventcard";
-// import { events } from "@/lib/constants";
+// const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+// import { IEvent } from "@/database";
+// import { cacheLife } from "next/cache";
+// import Eventcard from "@/components/Eventcard";
+// // import { events } from "@/lib/constants";
 
-const page = async () => {
-  "use cache";
-  cacheLife("seconds");
+// const page = async () => {
+//   "use cache";
+//   cacheLife("seconds");
 
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+//   const response = await fetch(`${BASE_URL}/api/events`);
+//   const { events } = await response.json();
     
-  return (
-    <div className="space-y-5">
-      <h3>Featured Events</h3>
-      <ul className="events">
-        {events &&
-          events.length > 0 &&
-          events.map((event: IEvent) => (
-            <li key={event.title} className="list-none">
-              <Eventcard {...event} />
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
-};
+//   return (
+//     <div className="space-y-5">
+//       <h3>Featured Events</h3>
+//       <ul className="events">
+//         {events &&
+//           events.length > 0 &&
+//           events.map((event: IEvent) => (
+//             <li key={event.title} className="list-none">
+//               <Eventcard {...event} />
+//             </li>
+//           ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-export default page;
+// export default page;
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
